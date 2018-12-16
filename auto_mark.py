9ef15605abfe6d1c1446b285d68996fc45c2fbea
Dinoyan <dinoyan.g98@gmail.com>
2018-12-16 14:26:41 -0500

    Changed cmd

--- auto_mark.py
+++ auto_mark.py
@@ -5,12 +5,13 @@ import os
 for file in os.listdir():
     if file.endswith(".c"):
         cmd = os.path.join(file)
-        resultFile = cmd[:-1] + "txt"
+        resultFile = cmd[:-2] + ".txt"
         subprocess.run(["gcc",cmd]) #For Compiling
         os.system("./a.out > " + resultFile)
-        subprocess.run(["FC", "resultFile", "solution.txt"])
-        mark = os.system("echo $?")
-        if (mark ==0):
+        mark = subprocess.run(["cmp", resultFile, "solution.txt"])
+        os.remove(resultFile)
+        mark = mark.returncode
+        if (mark == 0):
         	print("100%")
         else:
         	print("0.0%")
